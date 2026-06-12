5c57399a3a673e1f01bed224b67319db70e7bf43
0s3a5 <163148412+0s3a5@users.noreply.github.com>
2026-06-12 18:12:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ app.post('/api/eventos', async (req, res) => {
 // LEER EVENTOS
 app.get('/api/eventos', async (req, res) => {
     try {
-        const queryEventos = await pool.query('SELECT * FROM eventos');
+        const queryEventos = await pool.query('SELECT * FROM voluntariados');
         res.json(queryEventos.rows);
     } catch (err) {
         console.error('❌ Error en /api/eventos GET:', err.message);
